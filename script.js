e850f75ed69403d5d09afca34397924c0600ae6b
Collappo <163467283+Collappo@users.noreply.github.com>
2024-03-14 22:29:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,271 @@
+let color = "";
+
+let pixel = "";
+
+
+function ColorBlack () {
+    color = "#000000";
+    console.log(color);
+    return color;
+};
+
+function ColorRed () {
+    color = "#FF0000";
+    console.log(color);
+    return color;
+};
+
+function ColorOrange () {
+    color = "#ff6a00";
+    console.log(color);
+    return color;
+};
+
+function ColorYellow () {
+    color = "#f2ff00";
+    console.log(color);
+    return color;
+};
+
+function ColorGreen () {
+    color = "#00ff00";
+    console.log(color);
+    return color;
+};
+
+function ColorGreenUltra () {
+    color = "#007e11";
+    console.log(color);
+    return color;
+};
+
+function ColorCyan () {
+    color = "#00f2ff";
+    console.log(color);
+    return color;
+};
+
+function ColorBlue () {
+    color = "#00F";
+    console.log(color);
+    return color;
+};
+
+function ColorSzaryd () {
+    color = "#545454";
+    console.log(color);
+    return color;
+};
+
+function ColorBrown () {
+    color = "rgb(80, 60, 30)";
+    console.log(color);
+    return color;
+};
+
+function ColorKrem () {
+    color = "#eeb8b8";
+    console.log(color);
+    return color;
+};
+
+function ColorWhite () {
+    color = "#FFFFFF";
+    console.log(color);
+    return color;
+};
+
+
+
+function ChangeColor000 (color) {
+    document.getElementById("pixel-000").style.backgroundColor = color;
+};
+function ChangeColor001 (color) {
+    document.getElementById("pixel-001").style.backgroundColor = color;
+};
+function ChangeColor002 (color) {
+    document.getElementById("pixel-002").style.backgroundColor = color;
+};
+function ChangeColor003 (color) {
+    document.getElementById("pixel-003").style.backgroundColor = color;
+};
+function ChangeColor004 (color) {
+    document.getElementById("pixel-004").style.backgroundColor = color;
+};
+function ChangeColor005 (color) {
+    document.getElementById("pixel-005").style.backgroundColor = color;
+};
+function ChangeColor006 (color) {
+    document.getElementById("pixel-006").style.backgroundColor = color;
+};
+function ChangeColor007 (color) {
+    document.getElementById("pixel-007").style.backgroundColor = color;
+};
+function ChangeColor008 (color) {
+    document.getElementById("pixel-008").style.backgroundColor = color;
+};
+function ChangeColor009 (color) {
+    document.getElementById("pixel-009").style.backgroundColor = color;
+};
+function ChangeColor010 (color) {
+    document.getElementById("pixel-010").style.backgroundColor = color;
+};
+function ChangeColor011 (color) {
+    document.getElementById("pixel-011").style.backgroundColor = color;
+};
+function ChangeColor012 (color) {
+    document.getElementById("pixel-012").style.backgroundColor = color;
+};
+function ChangeColor013 (color) {
+    document.getElementById("pixel-013").style.backgroundColor = color;
+};
+function ChangeColor014 (color) {
+    document.getElementById("pixel-014").style.backgroundColor = color;
+};
+function ChangeColor015 (color) {
+    document.getElementById("pixel-015").style.backgroundColor = color;
+};
+function ChangeColor016 (color) {
+    document.getElementById("pixel-016").style.backgroundColor = color;
+};
+function ChangeColor017 (color) {
+    document.getElementById("pixel-017").style.backgroundColor = color;
+};
+function ChangeColor018 (color) {
+    document.getElementById("pixel-018").style.backgroundColor = color;
+};
+function ChangeColor019 (color) {
+    document.getElementById("pixel-019").style.backgroundColor = color;
+};
+function ChangeColor020 (color) {
+    document.getElementById("pixel-020").style.backgroundColor = color;
+};
+function ChangeColor021 (color) {
+    document.getElementById("pixel-021").style.backgroundColor = color;
+};
+function ChangeColor022 (color) {
+    document.getElementById("pixel-022").style.backgroundColor = color;
+};
+function ChangeColor023 (color) {
+    document.getElementById("pixel-023").style.backgroundColor = color;
+};
+function ChangeColor024 (color) {
+    document.getElementById("pixel-024").style.backgroundColor = color;
+};
+function ChangeColor025 (color) {
+    document.getElementById("pixel-025").style.backgroundColor = color;
+};
+function ChangeColor026 (color) {
+    document.getElementById("pixel-026").style.backgroundColor = color;
+};
+function ChangeColor027 (color) {
+    document.getElementById("pixel-027").style.backgroundColor = color;
+};
+function ChangeColor028 (color) {
+    document.getElementById("pixel-028").style.backgroundColor = color;
+};
+function ChangeColor029 (color) {
+    document.getElementById("pixel-029").style.backgroundColor = color;
+};
+function ChangeColor030 (color) {
+    document.getElementById("pixel-030").style.backgroundColor = color;
+};
+function ChangeColor031 (color) {
+    document.getElementById("pixel-031").style.backgroundColor = color;
+};
+function ChangeColor032 (color) {
+    document.getElementById("pixel-032").style.backgroundColor = color;
+};
+function ChangeColor033 (color) {
+    document.getElementById("pixel-033").style.backgroundColor = color;
+};
+function ChangeColor034 (color) {
+    document.getElementById("pixel-034").style.backgroundColor = color;
+};
+function ChangeColor035 (color) {
+    document.getElementById("pixel-035").style.backgroundColor = color;
+};
+function ChangeColor036 (color) {
+    document.getElementById("pixel-036").style.backgroundColor = color;
+};
+function ChangeColor037 (color) {
+    document.getElementById("pixel-037").style.backgroundColor = color;
+};
+function ChangeColor038 (color) {
+    document.getElementById("pixel-038").style.backgroundColor = color;
+};
+function ChangeColor039 (color) {
+    document.getElementById("pixel-039").style.backgroundColor = color;
+};
+function ChangeColor040 (color) {
+    document.getElementById("pixel-040").style.backgroundColor = color;
+};
+function ChangeColor041 (color) {
+    document.getElementById("pixel-041").style.backgroundColor = color;
+};
+function ChangeColor042 (color) {
+    document.getElementById("pixel-042").style.backgroundColor = color;
+};
+function ChangeColor043 (color) {
+    document.getElementById("pixel-043").style.backgroundColor = color;
+};
+function ChangeColor044 (color) {
+    document.getElementById("pixel-044").style.backgroundColor = color;
+};
+function ChangeColor045 (color) {
+    document.getElementById("pixel-045").style.backgroundColor = color;
+};
+function ChangeColor046 (color) {
+    document.getElementById("pixel-046").style.backgroundColor = color;
+};
+function ChangeColor047 (color) {
+    document.getElementById("pixel-047").style.backgroundColor = color;
+};
+function ChangeColor048 (color) {
+    document.getElementById("pixel-048").style.backgroundColor = color;
+};
+function ChangeColor049 (color) {
+    document.getElementById("pixel-049").style.backgroundColor = color;
+};
+function ChangeColor050 (color) {
+    document.getElementById("pixel-050").style.backgroundColor = color;
+};
+function ChangeColor051 (color) {
+    document.getElementById("pixel-051").style.backgroundColor = color;
+};
+function ChangeColor052 (color) {
+    document.getElementById("pixel-052").style.backgroundColor = color;
+};
+function ChangeColor053 (color) {
+    document.getElementById("pixel-053").style.backgroundColor = color;
+};
+function ChangeColor054 (color) {
+    document.getElementById("pixel-054").style.backgroundColor = color;
+};
+function ChangeColor055 (color) {
+    document.getElementById("pixel-055").style.backgroundColor = color;
+};
+function ChangeColor056 (color) {
+    document.getElementById("pixel-056").style.backgroundColor = color;
+};
+function ChangeColor057 (color) {
+    document.getElementById("pixel-057").style.backgroundColor = color;
+};
+function ChangeColor058 (color) {
+    document.getElementById("pixel-058").style.backgroundColor = color;
+};
+function ChangeColor059 (color) {
+    document.getElementById("pixel-059").style.backgroundColor = color;
+};
+function ChangeColor060 (color) {
+    document.getElementById("pixel-060").style.backgroundColor = color;
+};
+function ChangeColor061 (color) {
+    document.getElementById("pixel-061").style.backgroundColor = color;
+};
+function ChangeColor062 (color) {
+    document.getElementById("pixel-062").style.backgroundColor = color;
+};
+function ChangeColor063 (color) {
+    document.getElementById("pixel-063").style.backgroundColor = color;
+};
